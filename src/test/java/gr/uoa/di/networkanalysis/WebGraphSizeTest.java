0c3defedbb713ba4fc75cb2733165725232d8caa
Panagiotis Liakos <liakospanagiotis@gmail.com>
2020-03-14 12:35:09 +0200

    added checkstyle

--- src/test/java/gr/uoa/di/networkanalysis/WebGraphSizeTest.java
+++ src/test/java/gr/uoa/di/networkanalysis/WebGraphSizeTest.java
@@ -20,109 +20,101 @@ import it.unimi.dsi.webgraph.BVGraph;
 
 public class WebGraphSizeTest {
 
-	@Test
-	public void testWebGraphSize() throws IOException {
-		
-		InputStream fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
-		InputStream gzipStream = new GZIPInputStream(fileStream);
-		Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
-		BufferedReader buffered = new BufferedReader(decoder);
-		
-		File tempFile = File.createTempFile("temp", ".txt");
-		tempFile.deleteOnExit();
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-		String line = buffered.readLine();
-		while ((line = buffered.readLine()) != null) {
-			String[] splits = line.split("\\s");
-			writer.write(String.format("%s\t%s\n", splits[0], splits[1]));	
-		}
-	    writer.close();
-	    buffered.close();
-		
-	    String basename = "flickr-growth";
-	    
-		ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(new FileInputStream(tempFile), 0);
-		BVGraph.store(inputGraph, basename);
-		BVGraph bvgraph = BVGraph.load(basename);
-		System.out.println(bvgraph.numNodes());
-		System.out.println(bvgraph.numArcs());
-	}
+    @Test
+    public void testWebGraphSize() throws IOException {
 
-	@Test
-	public void testLLPWebGraphSize() throws IOException {
-		
-		InputStream fileStream = new FileInputStream("flickr-growth.llp.sorted.txt");
-		
-	    String basename = "flickr-growth-llp";
-	    
-		ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(fileStream, 0);
-		BVGraph.store(inputGraph, basename);
-		BVGraph bvgraph = BVGraph.load(basename);
-		System.out.println(bvgraph.numNodes());
-		System.out.println(bvgraph.numArcs());
-	}
+        InputStream fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
+        InputStream gzipStream = new GZIPInputStream(fileStream);
+        Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
+        BufferedReader buffered = new BufferedReader(decoder);
 
-	
-	@Test
-	public void computeLLPPermutation() throws FileNotFoundException, IOException{
-		
-		InputStream fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
-		InputStream gzipStream = new GZIPInputStream(fileStream);
-		Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
-		BufferedReader buffered = new BufferedReader(decoder);
-		
-		File tempFile = File.createTempFile("temp", ".txt");
-		tempFile.deleteOnExit();
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-		String line = buffered.readLine();
-		while ((line = buffered.readLine()) != null) {
-			String[] splits = line.split("\\s");
-			writer.write(String.format("%s\t%s\n", splits[0], splits[1]));	
-		}
-	    writer.close();
-	    buffered.close();
-		
-	    String basename = "flickr-growth";
-	    
-		ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(new FileInputStream(tempFile), 0);
-		BVGraph.store(inputGraph, basename);
-		
-		BVGraph bvgraph = BVGraph.load(basename);
-		
-//		ig = Transform.filterArcs(ig, Transform.NO_LOOPS);
-//		ig = Transform.symmetrize(ig, new ProgressLogger());
-		
-		LayeredLabelPropagation llp = new LayeredLabelPropagation(bvgraph, 0);
-		
-		int[] map = llp.computePermutation(new double[]{-5,-2,-.5,-.1,.001,.05,.1,.15,0.2,.3,.5,.8,1,2,3}, null);
+        File tempFile = File.createTempFile("temp", ".txt");
+        tempFile.deleteOnExit();
 
-		
-		File file = new File(basename + ".llp.txt");
-		// if file doesnt exists, then create it
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		FileWriter fw = new FileWriter(file.getAbsoluteFile());
-		BufferedWriter bw = new BufferedWriter(fw);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        String line = buffered.readLine();
+        while ((line = buffered.readLine()) != null) {
+            String[] splits = line.split("\\s");
+            writer.write(String.format("%s\t%s\n", splits[0], splits[1]));
+        }
+        writer.close();
+        buffered.close();
 
-		fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
-		gzipStream = new GZIPInputStream(fileStream);
-		decoder = new InputStreamReader(gzipStream, "UTF-8");
-		buffered = new BufferedReader(decoder);
-		
-		writer = new BufferedWriter(new FileWriter(tempFile));
-		line = buffered.readLine();
-		while ((line = buffered.readLine()) != null) {
-			String[] splits = line.split("\\s");
-			//write symmetric edges
-	       	bw.write(map[Integer.parseInt(splits[0])]+" "+map[Integer.parseInt(splits[1])]+" "+splits[2]+"\n");
-		}
-		
-		bw.close();
-		buffered.close();
+        String basename = "flickr-growth";
 
-	}	
-	
+        ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(new FileInputStream(tempFile), 0);
+        BVGraph.store(inputGraph, basename);
+        BVGraph bvgraph = BVGraph.load(basename);
+        System.out.println(bvgraph.numNodes());
+        System.out.println(bvgraph.numArcs());
+    }
+
+    public void testLLPWebGraphSize() throws IOException {
+
+        InputStream fileStream = new FileInputStream("flickr-growth.llp.sorted.txt");
+
+        String basename = "flickr-growth-llp";
+
+        ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(fileStream, 0);
+        BVGraph.store(inputGraph, basename);
+        BVGraph bvgraph = BVGraph.load(basename);
+        System.out.println(bvgraph.numNodes());
+        System.out.println(bvgraph.numArcs());
+    }
+
+
+    public void computeLLPPermutation() throws FileNotFoundException, IOException{
+
+        InputStream fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
+        InputStream gzipStream = new GZIPInputStream(fileStream);
+        Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
+        BufferedReader buffered = new BufferedReader(decoder);
+
+        File tempFile = File.createTempFile("temp", ".txt");
+        tempFile.deleteOnExit();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        String line = buffered.readLine();
+        while ((line = buffered.readLine()) != null) {
+            String[] splits = line.split("\\s");
+            writer.write(String.format("%s\t%s\n", splits[0], splits[1]));
+        }
+        writer.close();
+        buffered.close();
+
+        String basename = "flickr-growth";
+
+        ArcListASCIIGraph inputGraph = new ArcListASCIIGraph(new FileInputStream(tempFile), 0);
+        BVGraph.store(inputGraph, basename);
+
+        BVGraph bvgraph = BVGraph.load(basename);
+
+        LayeredLabelPropagation llp = new LayeredLabelPropagation(bvgraph, 0);
+
+        int[] map = llp.computePermutation(new double[]{-5,-2,-.5,-.1,.001,.05,.1,.15,0.2,.3,.5,.8,1,2,3}, null);
+
+        File file = new File(basename + ".llp.txt");
+        // if file doesnt exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
+        gzipStream = new GZIPInputStream(fileStream);
+        decoder = new InputStreamReader(gzipStream, "UTF-8");
+        buffered = new BufferedReader(decoder);
+
+        writer = new BufferedWriter(new FileWriter(tempFile));
+        line = buffered.readLine();
+        while ((line = buffered.readLine()) != null) {
+            String[] splits = line.split("\\s");
+            bw.write(map[Integer.parseInt(splits[0])]+" "+map[Integer.parseInt(splits[1])]+" "+splits[2]+"\n");
+        }
+
+        bw.close();
+        buffered.close();
+
+    }
 }
