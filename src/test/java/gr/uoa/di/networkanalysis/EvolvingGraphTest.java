76ed1bbdbc1f3e70da033b94327b5200cb4facc9
theodore <theo.stefou@gmail.com>
2020-07-14 16:36:03 +0300

    Now uses RandomAccessFile and added an index for the file
    RandomAccessFile: To be able to retrieve the current file position of
    timestamps.txt
    
    Index: ArrayList which keeps the offset to each line of the file using the positions provided from the RandomAccessFile

--- src/test/java/gr/uoa/di/networkanalysis/EvolvingGraphTest.java
+++ src/test/java/gr/uoa/di/networkanalysis/EvolvingGraphTest.java
@@ -8,8 +8,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
@@ -20,17 +23,15 @@ import it.unimi.dsi.webgraph.BVGraph;
 
 public class EvolvingGraphTest {
 
-	public void writeTimestampsToFile(int currentNode, int[] neighbors, int outDegree, Map<Integer,Long> currentNeighborsWithTimestamps, BufferedWriter writer) throws IOException{
-		// TODO Remove currentNode, it is not needed, I just added it for testing
-		writer.append(Integer.toString(currentNode));
-    	writer.append(' ');
-    	for(int i=0; i < outDegree; i++) {
-    		writer.append(Long.toString(currentNeighborsWithTimestamps.get(neighbors[i])));
+	public void writeTimestampsToFile(int[] neighbors, int outDegree, Map<Integer,Long> currentNeighborsWithTimestamps, RandomAccessFile raf) throws IOException {
+
+		for(int i=0; i < outDegree; i++) {
+    		raf.writeBytes(Long.toString(currentNeighborsWithTimestamps.get(neighbors[i])));
     		if(i != outDegree-1) {
-    			writer.append(' ');
+    			raf.writeBytes(" ");
     		}
     		else {
-    			writer.newLine();
+    			raf.writeBytes("\n");
     		}
     	}
 	}
@@ -61,9 +62,7 @@ public class EvolvingGraphTest {
         BVGraph bvgraph = BVGraph.load(basename);
         System.out.println(bvgraph.numNodes());
         System.out.println(bvgraph.numArcs());
-        
-        System.out.println("Here comes my code to ruin the day!");
-        
+                
         fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
         gzipStream = new GZIPInputStream(fileStream);
         decoder = new InputStreamReader(gzipStream, "UTF-8");
@@ -72,7 +71,10 @@ public class EvolvingGraphTest {
         // Skip the header of the file
         buffered.readLine();
         // The file we will write the results to
-        writer = new BufferedWriter(new FileWriter("timestamps.txt"));
+        RandomAccessFile raf = new RandomAccessFile("timestamps.txt", "rw");
+        // Maintain an index of positions in the file for each node -> timestamps line
+        // The number of nodes is known beforehand, set initial capacity accordingly
+        List<Long> offsetsIndex = new ArrayList<Long>(bvgraph.numNodes());
         // Start reading the file
         int currentNode = 1;
         Map<Integer,Long> currentNeighborsWithTimestamps = new HashMap<Integer,Long>(); // neighbor -> timestamp
@@ -80,13 +82,22 @@ public class EvolvingGraphTest {
             String[] tokens = line.split("\\s+");
             int node = Integer.parseInt(tokens[0]);
             int neighbor = Integer.parseInt(tokens[1]);
-            long timestamp =  Long.parseLong(tokens[3]);
+            long timestamp = Long.parseLong(tokens[3]);
+            // If at least one node was skipped, add that many empty lines to the file and update the index accordingly
+            if(node > currentNode + 1) {
+            	for(int i = 0; i < node - currentNode - 1; i++) {
+            		offsetsIndex.add(raf.getFilePointer());
+            		raf.writeBytes("\n");
+            	}
+            }
+
+            // If you find a new currentNode in the file, write the results you have so far about the current node.
             if(node != currentNode) {
-            	// If you find a new currentNode in the file, write the results you have so far about the current node.
             	// First get the sequence of neighbors from BVGraph
             	int[] neighbors = bvgraph.successorArray(currentNode);
             	int outDegree = bvgraph.outdegree(currentNode); // successorArray might return extra results, only the [0,outDegree) range is valid
-            	writeTimestampsToFile(currentNode, neighbors, outDegree, currentNeighborsWithTimestamps, writer);
+            	offsetsIndex.add(raf.getFilePointer());
+            	writeTimestampsToFile(neighbors, outDegree, currentNeighborsWithTimestamps, raf);
             	
             	// Prepare the variables for the next currentNode
             	currentNode = node;
@@ -101,9 +112,19 @@ public class EvolvingGraphTest {
         // Write the last node. It was not written because no change in node != currentNode was detected
         int[] neighbors = bvgraph.successorArray(currentNode);
     	int outDegree = bvgraph.outdegree(currentNode);
-        writeTimestampsToFile(currentNode, neighbors, outDegree, currentNeighborsWithTimestamps, writer);
+    	offsetsIndex.add(raf.getFilePointer());
+        writeTimestampsToFile(neighbors, outDegree, currentNeighborsWithTimestamps, raf);
         
-        writer.close();
+        raf.close();
         buffered.close();
+        
+        // At this point, if raf was not closed, you could seek any node using the offsetsIndex list which holds the offsets
+        /* 
+         * e.g. To find the timestamps of the node i:
+         * 
+         * raf.seek(offsetsIndex.get(i));
+         * String[] timestamps = raf.readLine().split(" "); returns the timestamps as string tokens 
+        	
+		*/
 	}
 }
