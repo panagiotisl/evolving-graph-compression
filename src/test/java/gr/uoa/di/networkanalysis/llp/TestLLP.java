4a1f3f6b4fe4ff3969b2f1bf05655ceec4f680be
TheoStefou <theo.stefou@gmail.com>
2020-10-27 15:18:38 +0200

    llp permutations any graph

--- src/test/java/gr/uoa/di/networkanalysis/llp/TestLLP.java
+++ src/test/java/gr/uoa/di/networkanalysis/llp/TestLLP.java
@@ -6,14 +6,53 @@ import it.unimi.dsi.webgraph.BVGraph;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 
 public class TestLLP {
 
+    // Flickr
+	private static final String graphFile =  "out.flickr-growth.sorted.gz";
+	private static final String basename =  "flickr";
+	private static final boolean headers = true;
+	private static final int k = 2;
+	private static int aggregation = 24*60*60;
+
+    // Wiki
+//	private static final String graphFile =  "out.edit-enwiki.sorted.gz";
+//	private static final String basename =  "wiki";
+//	private static final boolean headers = true;
+//	private static final int k = 2;
+//	private static int aggregation = 60*60;
+
+    // Yahoo
+//	private static final String graphFile =  "yahoo-G5-sorted.tsv.gz";
+//	private static final String basename =  "yahoo";
+//	private static final boolean headers = false;
+//	private static final int k = 2;
+//	private static int aggregation = 15*60;
+
+    // cbtComm
+//    private static final String graphFile =  "cbtComm-sorted.txt.gz";
+//    private static final String basename =  "cbtComm";
+//    private static final boolean headers = false;
+//    private static final int k = 2;
+//    private static int aggregation = 1;
+
+    // cbtPow
+//	private static final String graphFile =  "cbtPow-sorted.txt.gz";
+//	private static final String basename =  "cbtPow";
+//	private static final boolean headers = false;
+//	private static final int k = 2;
+//	private static int aggregation = 1;
+
     @Test
     public void computeLLPPermutation() throws FileNotFoundException, IOException {
 
-        InputStream fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
+        ClassLoader classLoader = getClass().getClassLoader();
+        String graphFileResourcePath = classLoader.getResource(graphFile).getPath();
+
+        InputStream fileStream = new FileInputStream(graphFileResourcePath);
         InputStream gzipStream = new GZIPInputStream(fileStream);
         Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
         BufferedReader buffered = new BufferedReader(decoder);
@@ -40,6 +79,15 @@ public class TestLLP {
         LayeredLabelPropagation llp = new LayeredLabelPropagation(bvgraph, 23);
 
         int[] map = llp.computePermutation(new double[]{.05,.1,.15,0.2}, null);
+        int[] reverse_map = new int[map.length];
+        for(int i = 0; i < map.length; i++) {
+            reverse_map[map[i]] = i;
+        }
+
+        System.out.println(map[100]);
+        System.out.println(reverse_map[map[100]]);
+
+        if(true) return;
 
         File file = new File(basename + ".llp.txt");
         // if file doesnt exists, then create it
@@ -49,7 +97,7 @@ public class TestLLP {
         FileWriter fw = new FileWriter(file.getAbsoluteFile());
         BufferedWriter bw = new BufferedWriter(fw);
 
-        fileStream = new FileInputStream("out.flickr-growth.sorted.gz");
+        fileStream = new FileInputStream(graphFileResourcePath);
         gzipStream = new GZIPInputStream(fileStream);
         decoder = new InputStreamReader(gzipStream, "UTF-8");
         buffered = new BufferedReader(decoder);
